fef158f809fd8bf8acd2cba7985b58359ced6222
Blendy <63923395+BlendyDev@users.noreply.github.com>
2020-10-11 16:28:37 +0200

    Add files via upload

--- Megascript.py
+++ Megascript.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
+from math import pi
+import time
 import re
-
-driver = webdriver.Chrome()
-driver.get('https://web.whatsapp.com/')
+import datetime
 
 print ("")
 print ("-------------------------------------------------------")
@@ -10,40 +10,69 @@ print ("- [MEGA-SCRIPT] - By BlendyDev (github.com/blendydev) -")
 print ("-------------------------------------------------------")
 print ("")
 print ("# FIRST SELECT A CONTACT/GROUP TO MESSAGE ON WHATSAPP WEB #")
+print ("")
 
 
+ChromeOptions = webdriver.ChromeOptions()
+ChromeOptions.add_argument('--no-sandbox') 
+ChromeOptions.add_argument('--user-data-dir=./User_Data')
+    
+driver = webdriver.Chrome(options=ChromeOptions)
+driver.get('https://web.whatsapp.com/')
+
+input('Press return after on group after scanning QR code | ')
+delay = 0.0
 def main ():
+    
+    # Fibonacci | Power | Consecutive | TextSpam | Send | FromTxt | Sticker | Goto | Pi | Delay
     print ("")
-    mode = input ('Enter a mode (Fibonacci | Power | Consecutive | TextSpam | Send | FromTxt | Sticker) : ')
-    if (mode.lower() != 'send' and mode.lower() != 'fromtxt'):
-        count = int(input('Enter the amount of messages : '))
+    global delay
+    mode = input ('Enter a mode (Type "help" for help) : ')
+    if (mode.lower() == 'Fibonacci' or mode.lower() == 'consecutive' or mode.lower().split(' ')[0] == 'textspam' or mode.lower() == 'sticker' or mode.lower() == 'power' or mode.lower() == 'fibonacci'):
+        isint = False
+        while (isint == False):
+            try:
+                count = int(input('Enter the amount of messages : '))
+                isint = True
+            except:
+                print ('Thats not a number...')
+        
 
-    input('Enter anything after on group after scanning QR code')
+    if (mode.lower() != 'help'):
+        msg_box = driver.find_element_by_xpath('//div[@data-tab="1"]')
 
+    def send (msg, s):
+        if (msg != ''):
+            msg_box.send_keys(msg)
+            button = driver.find_element_by_class_name('_1U1xa')
+            button.click()
+            if (s > 0):
+                time.sleep(s)
 
-
-    msg_box = driver.find_element_by_xpath('//div[@data-tab="1"]')
-
-    def send (msg):
-        msg_box.send_keys(msg)
-        button = driver.find_element_by_class_name('_1U1xa')
-        button.click()
-
-    if (mode.lower() == "textspam"):
+    if (mode.lower().split (' ')[0] == "textspam"):
+        phmode = False
+        if (len(mode.lower().split(' ')) > 1):
+            if (mode.lower().split(' ')[1] == '-p'):
+                phmode = True
         msg = input ('Enter text : ')
         for i in range(count):
-            send(msg)
+            if (phmode == True):
+                date = datetime.datetime.now()
+                msg = msg.replace ("$SECOND", str(date.second)) .replace("$MINUTE", str(date.minute)).replace ("$HOUR", str(date.hour)).replace ("$DAY", str(date.day)).replace ("$MONTH", str(date.month)).replace ("$YEAR", str(date.year))
+            send(msg, delay)
+    elif (mode.lower() == 'pi'):
+            send (str(pi)[0:int(input('Number of digits : '))], delay)
     elif (mode.lower() == 'consecutive'):
         startingnum = int(input ('Starting on : '))
         num = startingnum
         for i in range(count):
-            send (str (num))
+            send (str (num), delay)
             num += 1
     elif (mode.lower() == 'power'):
         power = int (input ('Power of : '))
         n = power
         for i in range(count):
-            send (str (n))
+            send (str (n), delay)
             n = n*power
     elif (mode.lower() == 'fibonacci'):
         a = 1
@@ -51,25 +80,35 @@ def main ():
         mode = 'a'
         for i in range(count):
             if (mode == 'a'):
-                send (str(a))
+                send (str(a), delay)
                 mode = 'b'
                 a = a+b
             elif (mode == 'b'):
-                send (str(b))
+                send (str(b), delay)
                 mode = 'a'
                 b = a+b
     elif (mode.lower() == 'send'):
         msg = input ('Type the message : ')
-        send (msg)
-    elif (mode.lower() == 'fromtxt'):
+        send (msg, 0)
+    elif (mode.lower().split(' ')[0] == 'fromtxt'):
+        
         file = open (input('Type the name of the file (Current dir, no ".txt") : ') + ".txt")
+        
         msg = re.sub(' +', ' ', file.read()).replace ("\n", "")
+        
         words = msg.split (" ")
         for i in range(len(words)):
             if (words[i].isspace()):
-                send("-")
+                send("-", delay)
             else:
-                send (words[i])
+                send (words[i], delay)
+    elif (mode.lower() == 'goto'):
+        chat = input("Insert the name of the group/chat : ")
+        try:
+            driver.find_element_by_xpath('//span[@title = "{}"]'.format(chat)).click()
+        except:
+            print('Unable to find element "' + chat + '"')
+
     elif (mode.lower() == 'sticker'):
         classname = input('Sticker url (blob:https://web.whatsapp...) : ')
         print (classname)
@@ -77,6 +116,31 @@ def main ():
         sticker = driver.find_element_by_xpath('//img[@src="' + classname + '"]')
         for i in range(count):
             sticker.click()
+    elif (mode.lower () == 'delay'):
+        try:
+            n = float(input('Set delay (seconds) : '))
+            if (n > 0):
+                delay = n
+            elif (n == 0):
+                delay = 0
+            else:
+                print ('Enter a number higher than 0!')
+        except:
+            print ('That\'s not a number!')
+    elif (mode.lower() == 'help'):
+        print ('''First run the script
+
+Connect to web.whatsapp.com and enter a chat (group/private)
+
+Select an option (Fibonacci | Power | Consecutive | TextSpam | Send | FromTxt | Sticker | Goto | Pi | Delay)
+
+Follow the steps
+
+For Sticker option:
+
+You need to press F12 or enter inspect mode and Inspect the sticker you want. There is gonna be a <img src="blob:https://web.whatsapp.com/RANDOMCHARS"> where you copy blob:https://web.whatsapp.com/RANDOMCHARS and paste it into the sticker url
+
+''')
         
     else:
         print ("That's not implemented yet...")
