2107a435e82475391b46d5dcbc9adaf7761fd5e4
Blendy <63923395+BlendyDev@users.noreply.github.com>
2020-10-11 16:43:13 +0200

    Add files via upload

--- Megascript.py
+++ Megascript.py
@@ -61,7 +61,7 @@ def main ():
                 message = msg.replace ("$SECOND", str(date.second)) .replace("$MINUTE", str(date.minute)).replace ("$HOUR", str(date.hour)).replace ("$DAY", str(date.day)).replace ("$MONTH", str(date.month)).replace ("$YEAR", str(date.year))
             else:
                 message = msg
-            send(messages, delay)
+            send(message, delay)
     elif (mode.lower() == 'pi'):
             send (str(pi)[0:int(input('Number of digits : '))], delay)
     elif (mode.lower() == 'consecutive'):
